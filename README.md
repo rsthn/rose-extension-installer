1776c5d9e9ccf48f6e2c757de2de5db8ab2fd115
Jonathan Palencia <strikeone88@rsthn.com>
2020-05-13 20:01:49 -0600

    Importing source code into repository.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rose Extension Installer for Composer
+
+Extension installer plugin for Composer, handled the `rose-extension` package types.
---
+++ src/ExtensionInstaller.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Rose\Composer;
+
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
+
+class ExtensionInstaller extends LibraryInstaller
+{
+	private function getExtensionName ($packageName)
+	{
+        $result = null;
+        preg_match ('/rose-ext-([A-Za-z0-9_-]+)/', $packageName, $result, 0);
+        return sizeof($result) == 0 ? null : $result[1];
+	}
+
+    public function getInstallPath (PackageInterface $package)
+    {
+        $repositoryManager = $this->composer->getRepositoryManager();
+        if (!$repositoryManager)
+            throw new \InvalidArgumentException('Unable to get repository manager.');
+
+        $repos = $repositoryManager->getLocalRepository();
+		if (!$repos)
+			throw new \InvalidArgumentException('Unable to get local repository.');
+	
+		$host = $repos->findPackage('rsthn/rose-core');
+		if (!$host)
+			throw new \InvalidArgumentException('Host package "rsthn/rose-core" was not found.');
+
+        return $host->getTargetDir().'/Ext/'.$this->getExtensionName($package->getPrettyName());
+    }
+
+    public function supports ($packageType)
+    {
+        return 'rose-extension' === $packageType;
+    }
+};
